3907e21053cddf41db14da3a019a8929021fdf1d
Rolex <drolewski@gmail.com>
2023-09-27 19:49:51 +0200

    Mobile styling for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from '../components/header';
 
 export default function Home() {
   return (
-    <main className="mx-64 my-10 font-sans">
+    <main className="xl:mx-64 xl:my-10 mx-4 my-6 font-sans">
       <Header/>
       {/*<section id="about-me">O mnie</section>*/}
       {/*<Footer/>*/}
