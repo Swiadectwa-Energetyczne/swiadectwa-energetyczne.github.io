f1465a7037391b2c80f4c2ec290ce5484ac7b986
Rolex <drolewski@gmail.com>
2023-09-27 21:42:34 +0200

    Prepare page content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,10 @@
 export default function Footer() {
   return (
     <footer className="text-l font-medium bg-secondary-bg font text-font-secondary h-12 sm:h-32 flex flex-row items-center justify-center">
+      <span>Link do polityki prywatności na osobnej stronie</span>
+      <span>Email</span>
+      <span>Facebook</span>
+      <span>Instagram</span>
       <span className="xl:mx-64 xl:my-10 mx-4 my-6">Copyrights © <a href="https://www.linkedin.com/in/dominik-rolewski-01bba9155/" target="_blank">Dominik Rolewski</a></span>
     </footer>
   )
