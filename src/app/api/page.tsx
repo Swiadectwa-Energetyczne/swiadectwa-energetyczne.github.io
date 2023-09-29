d69e1e48300ed17c4a8223bb78b4e55df603ed7d
Rolex <drolewski@gmail.com>
2023-09-29 22:12:14 +0200

    Fix sendgrid integration

---
+++ src/app/api/page.tsx
@@ -0,0 +1,11 @@
+import { sendEmail } from '@/lib/send-grid';
+
+export default async (req, res) => {
+  try {
+    await sendEmail();
+    // res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error(error);
+    // res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
