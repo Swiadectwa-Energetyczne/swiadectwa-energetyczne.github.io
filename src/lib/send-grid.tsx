5bdfba44f436396b046d9223c705e754749432b9
Rolex <drolewski@gmail.com>
2023-09-29 22:28:38 +0200

    Fix sendgrid integration

--- src/lib/send-grid.tsx
+++ src/lib/send-grid.tsx
@@ -1,14 +1,13 @@
-import MailService from "@sendgrid/mail";
-
+import sgMail from '@sendgrid/mail';
 
 export const sendEmail = async () => {
   console.log(process.env.send_grid_api_key);
-  MailService.setApiKey(process.env.send_grid_api_key);
+  sgMail.setApiKey(process.env.send_grid_api_key || "");
   const msg = {
     to: "drolewski@gmail.com",
     from: 'swiadectwa.eko@gmail.com',
     subject: 'Test SD',
     text: 'TEST SENDGRID POZDRAWIAM SERDECZNIE xDDDDDDDDDDDDDDDDDDDDDDDDDDD',
   };
-  MailService.send(msg).catch(e => console.log(e));
+  sgMail.send(msg).catch(e => console.log(e));
 };
