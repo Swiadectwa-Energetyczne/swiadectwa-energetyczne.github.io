6491da76f337681ac80f2ff7ff023914952f969f
Rolex <drolewski@gmail.com>
2023-09-29 22:43:49 +0200

    Fix sendgrid integration

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,11 +1,28 @@
 import { sendEmail } from '@/lib/send-grid';
 
-export default async () => {
+type JSONProps = {
+  data?: string[];
+};
+
+export default function API (props: JSONProps) {
+  console.log(props.data);
   try {
-    await sendEmail();
+    // await sendEmail();
     // res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error(error);
     // res.status(500).json({ message: 'Internal Server Error' });
   }
 }
+
+export const getStaticProps = async () => {
+  if (process.env.send_grid_api_key)
+    return {
+      props: {
+        data: process.env.send_grid_api_key
+          ? ['secret', 'data']
+          : ['public', 'data'],
+      },
+    }
+  throw new Error("Secrets don't exist");
+};
