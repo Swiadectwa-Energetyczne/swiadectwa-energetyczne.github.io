07907a49517757cbf9552db1381d480449967ba0
Rolex <drolewski@gmail.com>
2023-10-07 21:56:08 +0200

    Add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import HeaderNavigation from '@/components/header-navigation';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="flex flex-row justify-between text-l font-semibold xl:mx-64 xl:my-10 mx-4 my-6">
-      <a href="/" className="py-2 px-5 rounded-full justify-self-center font-bold text-3xl lg:hover:bg-button-hover">Świadectwa Charakterystyki Energetycznej</a>
+      <a href="/" className="py-2 px-5 rounded-full justify-self-center font-bold text-3xl lg:hover:bg-button-hover"><Image src="logo.png" alt="test" width={256} height={32}/></a>
       <div className="lg:flex items-center space-x-5 text-xl hidden">
         {/*TODO burger button mobile*/}
         <HeaderNavigation href="#about" text="O mnie"/>
