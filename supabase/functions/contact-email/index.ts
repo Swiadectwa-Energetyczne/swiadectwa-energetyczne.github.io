577c59ccd0b036cb6dc43f88c7f12719acbb7352
Rolex <drolewski@gmail.com>
2023-10-08 17:52:03 +0200

    Configure email edge function

--- supabase/functions/contact-email/index.ts
+++ supabase/functions/contact-email/index.ts
@@ -4,7 +4,7 @@ import { corsHeaders } from '../_shared/cors.ts'
 const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
 
 const handler = async (_request: Request): Promise<Response> => {
-  console.log(_request)
+  const {email, name, surname, tel, post, subject, content} = await _request.json();
   const res = await fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: {
@@ -12,10 +12,21 @@ const handler = async (_request: Request): Promise<Response> => {
       Authorization: `Bearer ${RESEND_API_KEY}`,
     },
     body: JSON.stringify({
-      from: 'kontakt@swiadectwa-eko.pl',
-      to: 'drolewski@gmail.com',
-      subject: 'hello world',
-      html: '<strong>it works!</strong>',
+      from: `${email}`,
+      to: 'swiadectwa.eko.kontakt@gmail.com',
+      subject: `Zapytanie kontaktowe: ${subject}`,
+      html: ```
+            <h1>Nowe zapytanie o kontakt od: ${email}</h1>
+            <h2>Dane osobowe interesanta:</h2>
+            <h3>Imię: ${name}</h3>
+            <h3>Nazwisko: ${surname}</h3>
+            <h3>Numer telefonu: ${tel}</h3>
+            <h3>Email: ${email}</h3>
+            <h3>Adres pocztowy: ${post}</h3>
+            <h3>Temat wiadomości: ${subject}</h3>
+            <h3>Tresć wiadomości:</h3>
+            <h4>${content}</h4>
+        ```,
     }),
   })
 
