3a5c37c01d2f06ce247021fe998558872063011e
Rolex <drolewski@gmail.com>
2023-10-09 22:35:04 +0200

    Update hi section

--- src/section/hi.tsx
+++ src/section/hi.tsx
@@ -5,16 +5,12 @@ export const Hi = () => {
     <div className="font-bold text-xl lg:text-3xl mb-24 text-font-secondary">
       <div
         className="p-4 hidden lg:flex flex-col items-center bg-secondary-bg rounded-full">
-        <div className="flex flex-row">Cześć&nbsp;<Image src="grin-alt-white.svg" alt="smile" width={24} height={24}/>
-        </div>
-        <div>Potrzebujesz Świadectwo Charakterystyki Energetycznej?</div>
+        <div className="flex flex-row">Cześć, potrzebujesz Świadectwo Charakterystyki Energetycznej?</div>
         <div>Jeśli tak, zapraszam!</div>
       </div>
       <div
         className="p-4 lg:hidden flex flex-col bg-secondary-bg">
-        <div className="flex flex-row">Cześć&nbsp;<Image src="grin-alt-white.svg" alt="smile" width={24} height={24}/>
-        </div>
-        <div>Potrzebujesz Świadectwo Charakterystyki Energetycznej?</div>
+        <div className="flex flex-row">Cześć, potrzebujesz Świadectwo Charakterystyki Energetycznej?</div>
         <div>Jeśli tak, zapraszam!</div>
       </div>
     </div>
